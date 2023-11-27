0e1694780c761793ddfab91d27af0c60e901684b
nana <135185645+evanessscence@users.noreply.github.com>
2023-11-27 11:29:25 -0600

    Add files via upload

--- src/main/java/com/uam/calc/Grafico.java
+++ src/main/java/com/uam/calc/Grafico.java
@@ -5,13 +5,21 @@
 package com.uam.calc;
 
 
+import java.awt.Color;
 import org.apache.commons.math3.analysis.UnivariateFunction;
 import net.objecthunter.exp4j.Expression;
 import net.objecthunter.exp4j.ExpressionBuilder;
 import org.jfree.chart.ChartFactory;
 import org.jfree.chart.ChartFrame;
 import org.jfree.chart.JFreeChart;
+import org.jfree.chart.axis.ValueAxis;
+import org.jfree.chart.plot.IntervalMarker;
+import org.jfree.chart.plot.ValueMarker;
+import org.jfree.chart.plot.XYPlot;
+import org.jfree.chart.ui.Layer;
+import org.jfree.chart.ui.RectangleInsets;
 import org.jfree.data.xy.DefaultXYDataset;
+import org.jfree.data.xy.XYSeries;
 
 public class Grafico {
      static void graficar(String funcion) {
@@ -19,20 +27,29 @@ public class Grafico {
         DefaultXYDataset dataset = new DefaultXYDataset();
         double[][] data = generarDatosFuncion(funcion);
         dataset.addSeries(funcion, data);
-
+        
+        
         // Crear el gráfico
         JFreeChart chart = ChartFactory.createXYLineChart(
                 "Gráfico de la función", // Título del gráfico
                 "x", // Etiqueta del eje x
-                "f(x)", // Etiqueta del eje y
+                "y", // Etiqueta del eje y
                 dataset // Datos del gráfico
         );
+       // Obtener el objeto XYPlot del gráfico
+        XYPlot plot = (XYPlot) chart.getPlot();
 
+  // Crear el marcador para el intervalo en el eje Y
+        ValueAxis rangeAxis = plot.getRangeAxis();
+        IntervalMarker markerY = new IntervalMarker(0, 0.9);
+        markerY.setPaint(new Color(255, 0, 255, 100)); 
+        plot.addRangeMarker(markerY, Layer.BACKGROUND);
+    
         // Mostrar el gráfico en una ventana
         ChartFrame frame = new ChartFrame("Gráfico", chart);
         frame.pack();
         frame.setVisible(true);
-    }
+   }
 
     // Generar los datos de la función ingresada
  private static double[][] generarDatosFuncion(String funcion) {
